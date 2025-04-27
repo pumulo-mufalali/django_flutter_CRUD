72ad8740fed675f68cecd01b0654332b780a77f0
Pumulo Mufalali <crispumulo@gmail.com>
2025-04-27 23:49:35 +0200

    flutter model added

--- flutter_frontend/lib/note.dart
+++ flutter_frontend/lib/note.dart
@@ -1,4 +1,39 @@
-mixin Note {
+import 'dart:convert';
+
+class Note {
   int id;
   String note;
+
+  Note({
+    required this.id,
+    required this.note,
+  });
+
+  Note copyWith({
+    int? id,
+    String? note,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      note: note ?? this.note,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'note': note,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      note: map['note'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Note.fromJson(String source) => Note.fromMap(json.decode(source));
 }
